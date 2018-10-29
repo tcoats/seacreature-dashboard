1358846ef360a9d84c5ea0d2c242c703652d3a36
Thomas Coats <github@voodoolabs.net>
2018-10-30 10:50:46 +1300

    delta and total lines

--- dist/d995293b65603d3f0211a009f1b3e907.js
+++ dist/d995293b65603d3f0211a009f1b3e907.js
@@ -351,6 +351,42 @@ const h = require('snabbdom/h').default
 const ql = require('odoql2')
 const inject = require('injectinto')
 
+const deltaline = (values) => {
+  let min = +Infinity
+  let max = -Infinity
+  for (let v of values) {
+    if (v < min) min = v
+    if (v > max) max = v
+  }
+  const y = (v) => 13 - (v - min) / (max - min) * 11
+  const coords = values.map((v, i) => [(i / (values.length - 1)) * 98, y(v)])
+  const last = coords[coords.length - 1]
+  return h('svg.deltaline', { attrs: { viewBox: '0 0 100 15', height: 'auto', width: '100%', preserveAspectRatio: 'none' } }, [
+    h('polyline', { attrs: { points: coords.map((c) =>
+      `${c[0]},${c[1]}`).join(' ') } }),
+    h('circle', { attrs: { cx: last[0], cy: last[1], r: 2 } })
+  ])
+}
+
+const totalline = (values) => {
+  let min = 0
+  let max = 0
+  for (let v of values) {
+    if (v < min) min = v
+    if (v > max) max = v
+  }
+  const y = (v) => 48 - (v - min) / (max - min) * 46
+  const coords = values.map((v, i) => [(i / (values.length - 1)) * 98, y(v)])
+  const last = coords[coords.length - 1]
+  return h('svg.totalline', { attrs: { viewBox: '0 0 100 50', height: 'auto', width: '100%', preserveAspectRatio: 'none' } }, [
+    h('polyline', { attrs: { points: coords.map((c) =>
+      `${c[0]},${c[1]}`).join(' ') } }),
+    h('polygon', { attrs: { points: `${coords.map((c) =>
+      `${c[0]},${c[1]}`).join(' ')} 98,${y(0)} 0,${y(0)}` } }),
+    h('circle', { attrs: { cx: last[0], cy: last[1], r: 2 } })
+  ])
+}
+
 inject('page:default', ql.component({
   query: (state, params) => {
     return {}
@@ -468,18 +504,41 @@ inject('page:default', ql.component({
             ])
           ])
         ]),
-        h('div.block.r2.c1.w1.h1'),
-        h('div.block.r3.c1.w1.h1', [
+        h('div.block.r2.c1.w1.h1', [
           h('div.block-unit.full.centered.danger', h('div.block-unit', [
             h('div.block-value', '134'),
             h('div.block-value-caption', 't of PK screened yesterday')
           ]))
         ]),
-        h('div.block.r4.c1.w1.h1'),
         h('div.block.r1.c2.w1.h1'),
         h('div.block.r2.c2.w1.h1'),
-        h('div.block.r3.c2.w1.h1'),
-        h('div.block.r4.c2.w1.h1'),
+        h('div.block.r3.c1.w2.h2', [
+          h('div.block-title', [
+            h('h2', 'Production Rate'),
+            h('h3', 'Per day over the last month')
+          ]),
+          h('div.block-unit', [
+            h('table', [
+              h('tbody', [
+                h('tr', [
+                  h('th', 'Palm Kernel'),
+                  h('td.bar', h('div.bar.green', { style: { width: `${65 / 100 * 9.6}em` } })),
+                  h('td', '1.4kt')
+                ]),
+                h('tr', [
+                  h('th', 'Dried Distiller Grains'),
+                  h('td.deltaline', deltaline([6, 5, 4, 7, 8, 9, 3])),
+                  h('td', '27.5kt')
+                ]),
+                h('tr.danger', [
+                  h('th', 'Tapioca Meal'),
+                  h('td.bar', h('div.bar.green', { style: { width: `${100 / 100 * 9.6}em` } })),
+                  h('td', '1.9kt')
+                ])
+              ])
+            ])
+          ])
+        ]),
         h('div.block.r1.c3.w2.h1', [
           h('div.block-title', [
             h('h2', 'Product Stock'),
@@ -515,7 +574,7 @@ inject('page:default', ql.component({
           h('div.block-unit', [
             h('table', [
               h('tbody', [
-                h('tr', [
+                h('tr.danger', [
                   h('th', 'PK'),
                   h('td', h('span.green', '+1.4kt   ▲'))
                 ]),
@@ -537,8 +596,24 @@ inject('page:default', ql.component({
             h('div.block-value-caption', 't of PK screened yesterday')
           ]))
         ]),
-        h('div.block.r1.c8.w1.h1'),
-        h('div.block.r2.c8.w1.h1'),
+        h('div.block.r1.c8.w1.h1', [
+          h('div.block-title', [
+            h('h2', 'Production Rate'),
+            h('h3', 'Nationwide')
+          ]),
+          h('div.totalline', totalline([6, 5, 4, 7, 8, 9, 3]))
+        ]),
+        h('div.block.r2.c8.w1.h1', [
+          h('div.block-title', [
+            h('h2', 'Production Rate'),
+            h('h3', 'Nationwide')
+          ]),
+          h('div.block-unit', [
+            h('div.block-value', '134'),
+            h('div.block-value-caption', 't of PK screened yesterday')
+          ]),
+          h('div.deltaline', deltaline([6, 5, 4, 7, 8, 9, 3]))
+        ]),
         h('div.block.r3.c8.w1.h1'),
         h('div.block.r4.c8.w1.h1')
       ]
